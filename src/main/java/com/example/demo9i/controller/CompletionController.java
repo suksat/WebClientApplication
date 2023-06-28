ddbda70058582a5eb60f9da5b3a66f391818de7e
Sukumar Satyen <125384989+suksat@users.noreply.github.com>
2023-06-28 05:58:52 +0530

    v4

--- src/main/java/com/example/demo9i/controller/CompletionController.java
+++ src/main/java/com/example/demo9i/controller/CompletionController.java
@@ -41,3 +41,53 @@ public class CompletionController {
                 .map(String::trim);
     }
 }
+/*
+
+@Controller
+@RequestMapping("/completions")
+public class CompletionController {
+
+    private final WebClient webClient;
+
+    public CompletionController(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @PostMapping
+    @ResponseBody
+    public String getCompletion(@RequestParam("promptString") String promptString) {
+        CompletionRequest request = new CompletionRequest();
+        request.setModel("text-davinci-003");
+        request.setPrompt(promptString);
+        request.setTemperature(1);
+        request.setMax_tokens(256);
+        request.setTop_p(1);
+        request.setFrequency_penalty(0);
+        request.setPresence_penalty(0);
+
+        CompletionResponse completionResponse = webClient.post()
+                .uri("https://api.openai.com/v1/completions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(CompletionResponse.class)
+                .block();
+
+        return completionResponse != null ? completionResponse.getText() : null;
+    }
+}
+*/
+
+/*
+Explanation:
+
+I changed the class annotation from @RestController to @Controller.
+Added @ResponseBody annotation to the getCompletion() method to indicate that
+the return value should be used as the response body.
+Changed the return type from Mono<String> to String for the getCompletion() method.
+Used the .block() method to block to wait for the completion of the asynchronous Mono execution
+and obtain the completion response synchronously.
+Return the getText() value from the CompletionResponse object, or null if the response was null.
+Now, the CompletionController class has the @Controller annotation,
+and the getCompletion() method returns a String as the response body.
+*/
